131d64e718cc6fa1b714fca204c1ef1254e8dee4
Thunchanok Phutthaphaiboon <phut.thunchanok@gmail.com>
2025-02-27 23:47:50 +0100

    midnight update

--- Assignment 2/EPA133a-G16-A2_datacleaning/model/model.py
+++ Assignment 2/EPA133a-G16-A2_datacleaning/model/model.py
@@ -73,7 +73,7 @@ class BangladeshModel(Model):
         Warning: the labels are the same as the csv column labels
         """
 
-        df = pd.read_csv('../data/demo-1.csv')
+        df = pd.read_csv('../data/processed/demo_100.csv')
 
         # a list of names of roads to be generated
         roads = ['N1']
