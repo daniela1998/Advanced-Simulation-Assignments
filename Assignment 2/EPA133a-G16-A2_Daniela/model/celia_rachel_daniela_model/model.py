44422fd258feff01086c49c2141b28d867ad00ac
daniela <dannyriosmora@gmail.com>
2025-03-05 11:37:40 +0100

    semicolon

--- Assignment 2/EPA133a-G16-A2_Daniela/model/celia_rachel_daniela_model/model.py
+++ Assignment 2/EPA133a-G16-A2_Daniela/model/celia_rachel_daniela_model/model.py
@@ -213,7 +213,7 @@ class BangladeshModel(Model):
         """
         keys = self.data[0].keys()
         with open(filename, 'w', newline='') as output_file:
-            dict_writer = csv.DictWriter(output_file, fieldnames=keys)
+            dict_writer = csv.DictWriter(output_file, fieldnames=keys, sep=';')
             dict_writer.writeheader()
             dict_writer.writerows(self.data)
 
