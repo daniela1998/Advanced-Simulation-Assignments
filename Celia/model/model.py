7f8b165282a09875b54bb213cf1503972745a426
Celia Martínez Sillero <cmartinezsille@tudelft.nl>
2025-03-15 19:14:29 +0100

    Second attempt

--- Celia/model/model.py
+++ Celia/model/model.py
@@ -1,7 +1,7 @@
 from mesa import Model
 from mesa.time import BaseScheduler
 from mesa.space import ContinuousSpace
-from Celia.model.components import Source, Sink, SourceSink, Bridge, Link, Intersection
+from components import Source, Sink, SourceSink, Bridge, Link, Intersection
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import defaultdict
@@ -69,7 +69,7 @@ class BangladeshModel(Model):
         self.sources = []
         self.sinks = []
         # MODIFIED: Create an empty NetworkX graph
-        self.G = nx.Graph()
+        #self.G = nx.Graph()
         self.generate_model()
 
 
@@ -88,6 +88,10 @@ class BangladeshModel(Model):
         roads = ['N1', 'N2']
 
         df_objects_all = []
+
+        # FROM YAO: Initialize NetworkX graph
+        self.G_nx = nx.Graph()
+
         for road in roads:
             # Select all the objects on a particular road in the original order as in the cvs
             df_objects_on_road = df[df['road'] == road]
@@ -102,15 +106,25 @@ class BangladeshModel(Model):
                 3. put the path in reversed order and reindex
                 4. add the path to the path_ids_dict so that the vehicles can drive backwards too
                 """
-                path_ids = df_objects_on_road['id']
+                path_ids = df_objects_on_road['id'] #[100000,1000001,1000002,1000003,1000004,1000005,1000006,1000007,1000008,1000009,1000010,1000011,1000012,1000013]
                 path_ids.reset_index(inplace=True, drop=True)
-                self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
+                self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids #(00,12):[]
                 self.path_ids_dict[path_ids[0], None] = path_ids #if the destination is None, the path is straight
                 path_ids = path_ids[::-1]
                 path_ids.reset_index(inplace=True, drop=True)
                 self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
                 self.path_ids_dict[path_ids[0], None] = path_ids #if the destination is None, the path is straight
 
+                # Add nodes and edges to NetworkX graph
+                for _, row in df_objects_on_road.iterrows():
+                    self.G_nx.add_node(row['id'], pos=(row['lon'], row['lat']), model_type=row['model_type'])
+
+                for i in range(len(df_objects_on_road) - 1):
+                    self.G_nx.add_edge(df_objects_on_road.iloc[i]['id'], df_objects_on_road.iloc[i + 1]['id'],
+                                       weight=df_objects_on_road.iloc[i]['length'])
+                print(self.G_nx.nodes)
+                print(self.G_nx.edges)
+#(100000,1000025):[]
         # put back to df with selected roads so that min and max and be easily calculated
         df = pd.concat(df_objects_all)
         y_min, y_max, x_min, x_max = set_lat_lon_bound(
@@ -163,10 +177,10 @@ class BangladeshModel(Model):
                     self.space.place_agent(agent, (x, y))
                     agent.pos = (x, y)
                     # MODIFIED: Add node to NetworkX graph
-                    self.G.add_node(agent.unique_id, model_type=model_type, pos=(x, y), length=row['length'])
-        self.G=self.create_network(df)
+                    #self.G.add_node(agent.unique_id, model_type=model_type, pos=(x, y), length=row['length'])
+        #self.G=self.create_network(df)
 
-
+    '''
     def create_network(self, dataframe):
         #create a network based on the data
         #Add edges ensuring correct connection rules
@@ -189,7 +203,7 @@ class BangladeshModel(Model):
 
 
         return self.G
-
+    '''
     def get_shortest_path(self, source_node=1000000, target_node=1000013):
 
         try:
@@ -201,7 +215,7 @@ class BangladeshModel(Model):
             # Print the results
             print(f"Shortest path from {source_node} to {target_node}: {shortest_path}")
             print(f"Total distance: {total_distance} meters")
-            return shortest_path, total_distance
+            return shortest_path
         except nx.NetworkXNoPath:
             print(f"No path found between {source_node} and {target_node}.")
         except nx.NodeNotFound as e:
@@ -217,14 +231,15 @@ class BangladeshModel(Model):
             sink = self.random.choice(self.sinks)
             if sink is not source:
                 break
-            if not self.path_ids_dict:
-                self.path_ids_dict=self.get_shortest_path(source, sink)
+        print(source,sink)
+        if self.path_ids_dict[source,sink].empty: # if this is this (100000:1000025)=[]
+            self.path_ids_dict=self.get_shortest_path(source, sink) #(100000:1000025)=[a route]
 
         return self.path_ids_dict[source, sink]
 
     # TODO
     def get_route(self, source):
-        return self.get_straight_route(source)
+        return self.get_random_route(source)
 
     def get_straight_route(self, source):
         """
